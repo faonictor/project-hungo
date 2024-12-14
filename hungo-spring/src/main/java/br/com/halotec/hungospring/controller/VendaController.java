806ab2e73309aafae58ffed21bb57bba55d31f65
faonictor <57420959+faonictor@users.noreply.github.com>
2024-12-14 14:20:55 -0300

    produto completo e restante das classes

---
+++ hungo-spring/src/main/java/br/com/halotec/hungospring/controller/VendaController.java
@@ -0,0 +1,4 @@
+package br.com.halotec.hungospring.controller;
+
+public class VendaController {
+}
---
+++ hungo-spring/src/main/java/br/com/halotec/hungospring/service/ProdutoIsumoService.java
@@ -0,0 +1,4 @@
+package br.com.halotec.hungospring.service;
+
+public class ProdutoIsumoService {
+}
