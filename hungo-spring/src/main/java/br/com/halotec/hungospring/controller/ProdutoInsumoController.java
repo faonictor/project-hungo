4573b5def7fb8f0ab72c0d6961f4614ed80d366f
faonictor <jnictoraraujo@gmail.com>
2024-12-29 14:09:05 -0300

    ajuste tabela produtos e ações

--- hungo-spring/src/main/java/br/com/halotec/hungospring/controller/ProdutoInsumoController.java
+++ hungo-spring/src/main/java/br/com/halotec/hungospring/controller/ProdutoInsumoController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @CrossOrigin(origins = "*")
 @RestController
 public class ProdutoInsumoController {
@@ -18,14 +20,20 @@ public class ProdutoInsumoController {
         return produtoInsumoService.salvar(produtoInsumo);
     }
 
-    @GetMapping("/produto-insumo")
-    public Iterable<ProdutoInsumo> listarTodos() {
-        return produtoInsumoService.listarTodos();
-    }
+//    @GetMapping("/produto-insumo")
+//    public Iterable<ProdutoInsumo> listarTodos() {
+//        return produtoInsumoService.listarTodos();
+//    }
 
     @GetMapping("/produto-insumo/{id}")
     public ResponseEntity<ProdutoInsumo> buscarPorId(@PathVariable Long id) {
         return produtoInsumoService.buscarPorId(id);
+    }
+
+    @GetMapping("/produto-insumo")
+    public ResponseEntity<List<ProdutoInsumo>> buscarInsumosPorProdutoId(@RequestParam Long produtoId) {
+        List<ProdutoInsumo> produtoInsumos = produtoInsumoService.buscarInsumosPorProdutoId(produtoId);
+        return ResponseEntity.ok(produtoInsumos); // Retorna os insumos encontrados
     }
 
     @DeleteMapping("/produto-insumo/{id}")
--- hungo-spring/src/main/java/br/com/halotec/hungospring/service/ProdutoInsumoService.java
+++ hungo-spring/src/main/java/br/com/halotec/hungospring/service/ProdutoInsumoService.java
@@ -1,5 +1,6 @@
 package br.com.halotec.hungospring.service;
 
+import br.com.halotec.hungospring.entity.Produto;
 import br.com.halotec.hungospring.entity.ProdutoInsumo;
 import br.com.halotec.hungospring.repository.ProdutoInsumoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,10 +8,19 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class ProdutoInsumoService {
     @Autowired
     private ProdutoInsumoRepository produtoInsumoRepository;
+
+    // Lógica para buscar os insumos de um produto
+    public List<ProdutoInsumo> buscarInsumosPorProdutoId(Long produtoId) {
+        Produto produto = new Produto();
+        produto.setId(produtoId);  // Cria o objeto Produto apenas com o ID
+        return produtoInsumoRepository.findByProduto(produto);
+    }
 
     public Iterable<ProdutoInsumo> listarTodos() {
         return produtoInsumoRepository.findAll();
